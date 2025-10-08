f0502ef43f04514557d33c1f2e50682f52dfb12b
ozyurtf <ozyurtf@outlook.com>
2025-10-08 01:32:18 -0400

    Update run_ddp.sh: remove module loads and reduce resources for testing

--- run_ddp.sh
+++ run_ddp.sh
@@ -4,12 +4,9 @@
 #SBATCH --error=ddp_%j.err        # Standard error
 #SBATCH --ntasks=2                # Number of processes (match number of GPUs)
 #SBATCH --gres=gpu:2              # Number of GPUs
-#SBATCH --time=01:00:00           # Max run time (HH:MM:SS)
-#SBATCH --cpus-per-task=4         # CPUs per process 
-#SBATCH --mem=16G                 # Memory per node 
-
-module load cuda/12.2             # Load CUDA module 
-module load python/3.10           # Load Python module 
+#SBATCH --time=00:05:00           # Max run time (HH:MM:SS)
+#SBATCH --cpus-per-task=2         # CPUs per process 
+#SBATCH --mem=1GB                 # Memory per node 
 
 # Activate your environment
 source /gpfs/u/home/AICD/AICDnhns/.local/share/enroot/colosseum/opt/conda/etc/profile.d/conda.sh
